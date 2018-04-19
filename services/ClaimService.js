08e346b147a8fd4891116d817c33c412c3cb01e6
Chris <capozzic1@gmail.com>
2018-04-19 15:39:30 -0400

    New Initial commit. Added in controller/service

--- services/ClaimService.js
+++ services/ClaimService.js
@@ -1,9 +1,15 @@
 app.service('ClaimService', ClaimService);
 
-function ClaimService() {
+function ClaimService($http, $q) {
   return {
     getData: function() {
-      return "DOG";
+      return $q(function(resolve, reject) {
+        $http.get("https://raw.githubusercontent.com/capozzic1/tsa-claims/master/claimsv2.json").then(function(data) {
+          resolve(data.data)
+        }, function(err) {
+          reject('Could not load json');
+        })
+      })
     }
   }
 }
