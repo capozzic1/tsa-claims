02d4089236711abba2e0144e09bc05c3ea5b22b8
Chris <capozzic1@gmail.com>
2018-04-19 18:39:24 -0400

    Rename json file to fix API problem

--- services/ClaimService.js
+++ services/ClaimService.js
@@ -4,7 +4,7 @@ function ClaimService($http, $q) {
   return {
     getData: function() {
       return $q(function(resolve, reject) {
-        $http.get("https://raw.githubusercontent.com/capozzic1/tsa-claims/master/claimsv2.json").then(function(data) {
+        $http.get("https://raw.githubusercontent.com/capozzic1/tsa-claims/master/claimsv3.json").then(function(data) {
           resolve(data.data)
         }, function(err) {
           reject('Could not load json');
