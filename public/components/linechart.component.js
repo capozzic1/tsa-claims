1b41d84c3f6294182ae5d766de45e782179ba930
Chris <capozzic1@gmail.com>
2018-04-22 15:32:17 -0400

    Added in yDomain for more intervals on the y Axis

--- public/components/linechart.component.js
+++ public/components/linechart.component.js
@@ -5,37 +5,51 @@ app.component('linechart', {
     $scope.getData = (function() {
 
       ClaimService.getData().then(function(data) {
-        data[0]["values"] = "yolo";
+        //data[0]["values"] = "yolo";
         //console.log(data[0]);
-        var newData = [];
+        var coordinates = [];
+        var airLineName = [];
+        //console.log(data);
 
+        var coords;
         for (var i = 0; i < data.length; i++) {
-          data[i]["values"] = {
-            x: i,
-            y: data[i].monthlyLoss
-          };
-        }
-        console.log(data)
+          //console.log(i)
 
-        $scope.data = data
+          coords = {
+            x: i,
+            y: data[i].monthlyLoss,
+            label: data[i].key
+          }
+
+          coordinates.push(coords);
+
+        }
+
+        $scope.data = [
+          {
+            key: "Refer to X axis for airline names",
+            values: coordinates,
+            color: "blue"
+          }
+        ]
+
+        debugger;
       })
     })();
 
     $scope.options = {
       chart: {
         type: 'lineChart',
-        height: 500,
-        margin: {
-          top: 20,
-          right: 20,
-          bottom: 50,
-          left: 65
-        },
+        height: 700,
+        width: 5000,
         x: function(d) {
-          return d.values.x;
+          //console.log(d)
+          return d.x;
+          //return d[0].values[0].x;
         },
         y: function(d) {
-          return d.values.y;
+          return Number(d.y);
+          //return d[0].values.y;
         },
 
         color: d3.scale.category10().range(),
@@ -44,14 +58,22 @@ app.component('linechart', {
         clipVoronoi: false,
 
         xAxis: {
-          axisLabel: 'Average monthly loss of all airlines: '
+          axisLabel: "Monthly loss",
+          tickFormat: function(d) {
+            console.log(d)
+            var label = $scope.data[0].values[d].label;
+            return label;
+            //debugger;
+          },
+          axisLabelDistance: 100
         },
 
         yAxis: {
           axisLabel: 'Y Axis',
 
           axisLabelDistance: 0
-        }
+        },
+        yDomain: [0, 40000]
       }
     };
   }
