a31201cef5a86b6d7ab27391b390e8ff13077958
Chris <capozzic1@gmail.com>
2018-04-23 17:58:22 -0400

    Updated

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var port = process.env.PORT || 3000;
 var path = require('path');
 var cors = require('cors');
 app.use(cors());
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(__dirname + '/public/index.html');  load the single view file (angular will handle the page changes on the front-end)
+// });
 app.listen(port, function() {
   console.log("Listening on port " + port);
 })
