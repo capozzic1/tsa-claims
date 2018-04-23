9b816ebc176518b7bd61bb107985e5d8ce99758d
Chris <capozzic1@gmail.com>
2018-04-23 17:22:10 -0400

    Updated server to send index

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var cors = require('cors');
 app.use(cors());
 app.use(express.static('./public'));
 
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.listen(port, function() {
   console.log("Listening on port " + port);
 })
