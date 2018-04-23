f1e5d4d16c38e5fb09ba043c7d16b11b51e23367
Chris <capozzic1@gmail.com>
2018-04-23 18:26:21 -0400

    Updated

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var cors = require('cors');
 app.use(cors());
 app.use(express.static('public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+//
+// });
 app.listen(port, function() {
   console.log("Listening on port " + port);
 })
