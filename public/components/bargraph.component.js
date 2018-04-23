e7c972bc7d1b202be091976cf3f2aed9e33b09c4
Chris <capozzic1@gmail.com>
2018-04-23 13:05:06 -0400

    Add form for adding bar graph data

--- public/components/bargraph.component.js
+++ public/components/bargraph.component.js
@@ -5,13 +5,37 @@ app.component('bargraph', {
     'ClaimService',
     '$filter',
     function($scope, ClaimService, $filter) {
-      console.log(ClaimService)
+
+      $scope.newData = {
+        apCode: null,
+        claimsPerMonth: null,
+        mean: null,
+        stDev: null
+      };
+
+      $scope.addData = function() {
+        $scope.data[0].values.push($scope.newData);
+
+        $scope.sort();
+
+      }
+      //keeps the bargraph sorted by claimsPerMonth after adding new data
+      $scope.sort = function(data) {
+
+        if (data) {
+          data = $filter('orderBy')(data, '-claimsPerMonth');
+          return data;
+        } else {
+          $scope.data[0].values = $filter('orderBy')($scope.data[0].values, '-claimsPerMonth');
+
+        }
+
+      }
 
       $scope.getData = (function() {
         ClaimService.getData("bargraph").then(function(data) {
           //console.log(data)
-          data = $filter('orderBy')(data, '-claimsPerMonth')
-          console.log(data);
+          data = $scope.sort(data);
 
           $scope.data = [
             {
@@ -20,6 +44,11 @@ app.component('bargraph', {
               values: data
             }
           ]
+
+          console.log($scope.data[0]);
+
+          //{apCode: "JFK", claimsPerMonth: 237, mean: 0.06825, stDev: 26.231}
+
         })
       })()
 
@@ -60,73 +89,3 @@ app.component('bargraph', {
     }
   ]
 });
-
-// $scope.data = [
-//   {
-//     "key": "Series1",
-//     "color": "#d62728",
-//     "values": [
-//       {
-//         "label": "Group A",
-//         "value": -1.8746444827653
-//       }, {
-//         "label": "Group B",
-//         "value": -8.0961543492239
-//       }, {
-//         "label": "Group C",
-//         "value": -0.57072943117674
-//       }, {
-//         "label": "Group D",
-//         "value": -2.4174010336624
-//       }, {
-//         "label": "Group E",
-//         "value": -0.72009071426284
-//       }, {
-//         "label": "Group F",
-//         "value": -0.77154485523777
-//       }, {
-//         "label": "Group G",
-//         "value": -0.90152097798131
-//       }, {
-//         "label": "Group H",
-//         "value": -0.91445417330854
-//       }, {
-//         "label": "Group I",
-//         "value": -0.055746319141851
-//       }
-//     ]
-//   }, {
-//     "key": "Series2",
-//     "color": "#1f77b4",
-//     "values": [
-//       {
-//         "label": "Group A",
-//         "value": 25.307646510375
-//       }, {
-//         "label": "Group B",
-//         "value": 16.756779544553
-//       }, {
-//         "label": "Group C",
-//         "value": 18.451534877007
-//       }, {
-//         "label": "Group D",
-//         "value": 8.6142352811805
-//       }, {
-//         "label": "Group E",
-//         "value": 7.8082472075876
-//       }, {
-//         "label": "Group F",
-//         "value": 5.259101026956
-//       }, {
-//         "label": "Group G",
-//         "value": 0.30947953487127
-//       }, {
-//         "label": "Group H",
-//         "value": 0
-//       }, {
-//         "label": "Group I",
-//         "value": 0
-//       }
-//     ]
-//   }
-// ]
