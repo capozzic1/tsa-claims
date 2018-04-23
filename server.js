fccf68141363098ac6ec1d3c91b633ba79d72288
Chris <capozzic1@gmail.com>
2018-04-23 17:09:58 -0400

    Updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 var path = require('path');
 var cors = require('cors');
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 
 app.listen(port, function() {
   console.log("Listening on port " + port);
