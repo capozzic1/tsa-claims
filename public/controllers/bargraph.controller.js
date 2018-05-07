3089c02dac7b9818205cc6fb353b629738dd7aa7
Chris <capozzic1@gmail.com>
2018-05-07 00:11:56 -0400

    Refactored LineChartController into an ES6 class

--- public/controllers/bargraph.controller.js
+++ public/controllers/bargraph.controller.js
@@ -16,10 +16,10 @@ export default class BargraphController {
         type: 'multiBarHorizontalChart',
         height: 7000,
 
-        x: function(d) {
+        x: (d) => {
           return d.apCode;
         },
-        y: function(d) {
+        y: (d) => {
           return d.claimsPerMonth;
         },
         showControls: false,
@@ -30,19 +30,18 @@ export default class BargraphController {
         },
         yAxis: {
           axisLabel: 'Average claims per month',
-          tickFormat: function(d) {
+          tickFormat: (d) => {
             return d3.format(',.2f')(d);
           }
         },
         tooltip: {
-          contentGenerator: function(d) {
-            console.log(d.data)
+          contentGenerator: (d) => {
 
-            var apCode = "Airport code: " + d.data.apCode;
-            var mean = "Mean: " + d.data.mean;
-            var stDev = "Standard deviation: " + d.data.stDev;
+            const apCode = `Airport code: ${d.data.apCode}`;
+            const mean = `Mean: ${d.data.mean}`;
+            const stDev = `Standard Deviation: ${d.data.stDev}`;
 
-            var htmlString = "<div><h4>" + apCode + "</h4>" + "<h4>" + mean + "</h4>" + "<h4>" + stDev + "</h4>" + "</div>"
+            const htmlString = `<div><h4>${apCode}</h4><h4>${mean}</h4><h4>${stDev}</h4></div>`;
             return htmlString;
           }
         }
@@ -51,7 +50,11 @@ export default class BargraphController {
     this.init();
   }
 
-  addData() {}
+  addData() {
+    this.data[0].values.push(this.newData);
+
+    this.sort();
+  }
 
   init() {
     this.getData();
@@ -69,7 +72,7 @@ export default class BargraphController {
   getData() {
 
     this.ClaimService.getData("bargraph").then((data) => {
-      console.log(data)
+
       data = this.sort(data);
 
       this.data = [
@@ -86,90 +89,3 @@ export default class BargraphController {
 }
 
 BargraphController.$inject = ['ClaimService', '$filter'];
-// BargraphController.$inject = ['$scope', 'ClaimService', '$filter'];
-//
-// function BargraphController($scope, ClaimService, $filter) {
-//
-//   $scope.newData = {
-//     apCode: null,
-//     claimsPerMonth: null,
-//     mean: null,
-//     stDev: null
-//   };
-//   adds data to the bar graph
-//   $scope.addData = function() {
-//     $scope.data[0].values.push($scope.newData);
-//
-//     $scope.sort();
-//
-//   }
-//   keeps the bargraph sorted by claimsPerMonth after adding new data
-//   $scope.sort = function(data) {
-//
-//     if (data) {
-//       data = $filter('orderBy')(data, '-claimsPerMonth');
-//       return data;
-//     } else {
-//       $scope.data[0].values = $filter('orderBy')($scope.data[0].values, '-claimsPerMonth');
-//
-//     }
-//
-//   }
-//   gets data from the claim service
-//   $scope.getData = (function() {
-//     ClaimService.getData("bargraph").then(function(data) {
-//       console.log(data)
-//       data = $scope.sort(data);
-//
-//       $scope.data = [
-//         {
-//           key: "Average claims per month",
-//           "color": "#107dea",
-//           values: data
-//         }
-//       ]
-//
-//     })
-//   })()
-//   options for angular nvd3's bar graph
-//   $scope.options = {
-//     chart: {
-//       type: 'multiBarHorizontalChart',
-//       height: 7000,
-//
-//       x: function(d) {
-//         return d.apCode;
-//       },
-//       y: function(d) {
-//         return d.claimsPerMonth;
-//       },
-//       showControls: false,
-//       showValues: true,
-//       duration: 500,
-//       xAxis: {
-//         showMaxMin: false
-//       },
-//       yAxis: {
-//         axisLabel: 'Average claims per month',
-//         tickFormat: function(d) {
-//           return d3.format(',.2f')(d);
-//         }
-//       },
-//       custom tooltip content
-//       tooltip: {
-//         contentGenerator: function(d) {
-//           console.log(d.data)
-//
-//           var apCode = "Airport code: " + d.data.apCode;
-//           var mean = "Mean: " + d.data.mean;
-//           var stDev = "Standard deviation: " + d.data.stDev;
-//
-//           var htmlString = "<div><h4>" + apCode + "</h4>" + "<h4>" + mean + "</h4>" + "<h4>" + stDev + "</h4>" + "</div>"
-//           return htmlString;
-//         }
-//       }
-//     }
-//   }
-// }
-//
-// module.exports = BargraphController;
